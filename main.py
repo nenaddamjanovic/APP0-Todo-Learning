e45afaa2d7ea8269445a08ed8220266bcfc8756e
Piton <nenaddamjanovic@hotmail.rs>
2024-05-28 23:12:30 +0200

    commit update s16

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from functions import get_todos, write_todos
+import time
+
+#  -----------------------------------------------------------------------------
+now = time.strftime("%H:%M:%S, %d %b, %Y ")
+print(f"Now it is: {now}")
 
 while True:
     user_action = input("type add, show, edit, complete (then space and command) or exit: ")
@@ -6,12 +11,12 @@ while True:
 
     if user_action.startswith("add"):
         todo = user_action[4:]
-        todos = get_todos()  # Ne treba parametar, ima default
+        todos = get_todos()
         todos.append(todo + "\n")
-        write_todos(todos)  # Ne treba drugi parametar, ima default
+        write_todos(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()  # Ne treba parametar, ima default
+        todos = get_todos()
         for index, item in enumerate(todos):
             item = item.strip("\n")
             row = f"{index + 1}-{item}"
@@ -22,10 +27,10 @@ while True:
             number = int(user_action[5:])
             print(number)
             number = number - 1
-            todos = get_todos()  # Ne treba parametar, ima default
+            todos = get_todos()
             new_todo = input("enter new todo: ")
             todos[number] = new_todo + "\n"
-            write_todos(todos)  # Ne treba drugi parametar, ima default
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid, format is ACTION NUMBER")
             continue
@@ -33,11 +38,11 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos()  # Ne treba parametar, ima default
+            todos = get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
-            write_todos(todos)  # Ne treba drugi parametar, ima default
+            write_todos(todos)
             message = f"item {todo_to_remove} is completed and removed from list"
             print(message)
         except IndexError:
