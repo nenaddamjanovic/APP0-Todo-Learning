ed71e54b8e5bcbeff5fe9bf9df61827d7c1387dc
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-02 23:46:11 +0200

    commit S20 - web app completing items

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ st.title("my todo app")
 st.subheader("This is my todo app :)")
 st.write("You increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label=" ", placeholder="Enter a new todo...",
               on_change=add_todo, key="key_new_todo")
