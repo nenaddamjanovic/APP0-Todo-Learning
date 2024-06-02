c629a58b82d89bc4f918091aa7dfcd100a6c9d55
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-02 23:37:33 +0200

    commit S20 - web app adding new items

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ from functions import get_todos, write_todos
 
 todos = get_todos()
 
+def add_todo():
+    local_todo = st.session_state["key_new_todo"].strip() + "\n"
+    todos.append(local_todo)
+    write_todos(todos)
+
+
 st.title("my todo app")
 st.subheader("This is my todo app :)")
 st.write("You increase your productivity")
@@ -10,4 +16,7 @@ st.write("You increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a new todo...")
+st.text_input(label=" ", placeholder="Enter a new todo...",
+              on_change=add_todo, key="key_new_todo")
+
+st.session_state
